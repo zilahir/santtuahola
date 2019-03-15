09d236b3009ed7dec3d7e4af03163b2871e86b31
Richard Zilahi <zilahi@gmail.com>
2019-03-15 13:52:26 +0200

    video added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Player } from 'video-react';
+import styled from 'styled-components';
+import bg from './assets/bg.png';
+import video from './assets/santtu_d.mp4';
+import './App.scss';
+import "video-react/dist/video-react.css"; 
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Wrapper style={{backgroundImage: `url(${bg})`}}>
+          <Player
+            fluid={false}
+            playsInline            
+            src={video}
+          />
+        </Wrapper>
       </div>
     );
   }
